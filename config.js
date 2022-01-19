d768349328c610da46fb538ac91fce28069bf0af
JuaneGradoMedio <98039763+JuaneGradoMedio@users.noreply.github.com>
2022-01-19 16:31:51 +0100

    Discord Bot

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = "ODAxMjIwMjgzNDQ0NzU2NTYw.YAdgiQ.dBZ9L19wR4e55ipULvKQMlnvRUw";
+
+exports.PREFIX = '!';
+
+exports.OWNER_ID = "515559000666931210"
---
+++ events/client/ready.js
@@ -0,0 +1,8 @@
+const { PREFIX, LAVA_HOST, LAVA_PASSWORD, LAVA_PORT  } = require('../../config');
+const { MessageEmbed } = require("discord.js")
+
+module.exports = async bot => {
+    console.log(`${bot.user.username} is available now!`)
+    var activities = [ `${bot.guilds.cache.size} servers`, `${bot.users.cache.size} users!` ], i = 0;
+    
+};
